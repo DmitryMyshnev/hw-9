8f8f7a8d0761b0ed30488479b30f73b7cee7e8cb
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2021-07-30 15:15:16 +0300

    update

--- src/Task2/GetUsers.java
+++ src/Task2/GetUsers.java
@@ -12,32 +12,31 @@ import java.util.Scanner;
 public class GetUsers {
     private final String PATH = "src/Task2/user.json";
 
-    public List<User>  getData(String fileName){
+    public List<User> getData(String fileName) {
         List<User> listUsers = new ArrayList<>();
-        try ( Scanner scanner = new Scanner(new File(fileName))){
+        try (Scanner scanner = new Scanner(new File(fileName))) {
             while (scanner.hasNext()) {
-                String[] data =  scanner.nextLine().split(" ");
-                if(!data[0].equalsIgnoreCase("name") && !data[1].equalsIgnoreCase("age")) {
+                String[] data = scanner.nextLine().split(" ");
+                if (!data[0].equalsIgnoreCase("name") && !data[1].equalsIgnoreCase("age")) {
                     listUsers.add(new User(data[0], Integer.valueOf(data[1])));
                 }
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-    return listUsers;
+        return listUsers;
     }
-    public void createFileFromJson(List<User> users){
+
+    public void createFileFromJson(List<User> users) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(users);
         File file = new File(PATH);
 
-        try(FileWriter writeJson = new FileWriter(file)) {
+        try (FileWriter writeJson = new FileWriter(file)) {
 
-             writeJson.write(json);
-             writeJson.flush();
-        }
-        catch (Exception e){
+            writeJson.write(json);
+            writeJson.flush();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
--- src/Task3/FrequencyWord.java
+++ src/Task3/FrequencyWord.java
@@ -10,29 +10,28 @@ public class FrequencyWord {
     public FrequencyWord(String path) {
         this.path = path;
     }
-    public void getFrequency(){
+
+    public void getFrequency() {
         String[] line;
         Integer count = 1;
-        Map<String,Integer> map = new HashMap<>();
-        try (Scanner scanner = new Scanner(new File(path))){
-            while (scanner.hasNext()){
+        Map<String, Integer> map = new HashMap<>();
+        try (Scanner scanner = new Scanner(new File(path))) {
+            while (scanner.hasNext()) {
                 line = scanner.nextLine().split(" ");
                 for (String st : line) {
-                   if(map.containsKey(st)){
-                       count++;
-                       map.put(st,count);
-                   }
-                   else {
-                       map.put(st,1);
-                   }
+                    if (map.containsKey(st)) {
+                        count++;
+                        map.put(st, count);
+                    } else {
+                        map.put(st, 1);
+                    }
                 }
-                count =1;
+                count = 1;
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-         map.entrySet().stream().sorted(Map.Entry.<String,Integer>comparingByValue().reversed()).forEach(System.out :: println);
+        map.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).forEach(System.out::println);
 
     }
 }
