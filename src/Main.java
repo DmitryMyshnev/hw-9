04fa08194ba84b149f404cebd6ab2000fad9a871
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2021-07-30 15:13:04 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ import Task2.GetUsers;
 import Task3.FrequencyWord;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
        PhoneValidation phoneValidation = new PhoneValidation();
        System.out.println(phoneValidation.getValidationPhones("src/Task1/file.txt"));
 
-        GetUsers getUsers = new GetUsers();
+      /*  GetUsers getUsers = new GetUsers();
        List users =  getUsers.getData("src/Task2/file.txt");
         getUsers.createFileFromJson(users);
 
         FrequencyWord freq = new FrequencyWord("src/Task3/word.txt");
-        freq.getFrequency();
+        freq.getFrequency();*/
+        
     }
 }
--- src/Task1/PhoneValidation.java
+++ src/Task1/PhoneValidation.java
@@ -2,59 +2,35 @@ package Task1;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class PhoneValidation {
-    public String getValidationPhones(String fileName){
+    public String getValidationPhones(String fileName) {
         String gotPhones = new String();
         String result = new String();
         boolean isSuccessful = true;
-        char[] templatePhone = new char[]{'(','x','x','x',')',' ','x','x','x','-','x','x','x','x'};
-        char[] templatePhone2 = new char[]{'x','x','x','-','x','x','x','-','x','x','x','x'};
+        char[] templatePhone = new char[]{'(', 'x', 'x', 'x', ')', ' ', 'x', 'x', 'x', '-', 'x', 'x', 'x', 'x'};
+        char[] templatePhone2 = new char[]{'x', 'x', 'x', '-', 'x', 'x', 'x', '-', 'x', 'x', 'x', 'x'};
 
-        try (FileReader reader = new FileReader(fileName)){
+        try (FileReader reader = new FileReader(fileName)) {
             int c;
             while ((c = reader.read()) != -1) {
                 gotPhones += (char) c;
             }
-            String[] phones = gotPhones.split("\r\n");
-            for (String st : phones) {
-                if (st.length() == 14) {
-                    for (int i = 0; i < st.length(); i++) {
-                        if (i == 0 || i == 4 || i == 5 || i == 9) {
-                            if (st.charAt(i) != templatePhone[i]) {
-                                isSuccessful = false;
-                                break;
-                            }
-                        } else if (!Character.isDigit(st.charAt(i))) {
-                            isSuccessful = false;
-                            break;
-                        }
-                    }
-                }else {
-                    if (st.length() == 12) {
-                        for (int i = 0; i < st.length(); i++) {
-                            if (i == 3 || i == 7) {
-                                if (st.charAt(i) != templatePhone2[i]) {
-                                    isSuccessful = false;
-                                    break;
-                                }
-                            } else if (!Character.isDigit(st.charAt(i))) {
-                                isSuccessful = false;
-                                break;
-                            }
-                        }
-                    } else {
-                        isSuccessful = false;
-                    }
-                }
-                    if (isSuccessful) {
-                        result += st + "\n";
-                    }
-                    else {isSuccessful = true;}
+            Pattern pattern = Pattern.compile(("[(]\\d\\d\\d[)]\\s\\d\\d\\d[-]\\d\\d\\d\\d"));
+            Pattern pattern2 = Pattern.compile(("\\d\\d\\d[-]\\d\\d\\d[-]\\d\\d\\d\\d"));
+
+            Matcher matcher = pattern.matcher(gotPhones);
+            while (matcher.find()) {
+                result += matcher.group() + "\r\n";
             }
-        }
-        catch (IOException e){
+            matcher = pattern2.matcher(gotPhones);
+            while (matcher.find()) {
+                result += matcher.group() + "\r\n";
+            }
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
         return result;
